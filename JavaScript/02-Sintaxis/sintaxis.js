d3023a26d177934cbe8e8d835a79fd6d5d0a8822
hix-angelica-rojas <angelica.rojas@hixsa.com>
2023-01-31 22:35:58 -0600

    sintaxis file added

---
+++ JavaScript/02-Sintaxis/sintaxis.js
@@ -0,0 +1,12 @@
+const lista = [
+  'Angelica',
+  29,
+  true,
+  new Date(1993, 7, 20),
+  {
+    titulo: 'tituloFake',
+    autor: 'autorFake',
+    fecha: new Date(2023, 0, 31),
+    url: 'https://fakeurl.com'
+  },
+]
